f9248f2e71228ba85247eccc03247d05198a736e
Cliff Chiang <fromkeytoend@Cliffs-MacBook-Pro.local>
2026-01-07 20:13:24 +0800

    Update base path for TRPGWorld repository

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue';
 function getBase() {
   /**
    * about VITE_TARGET_ENV
-   *  - pages: for GitHub Pages - hash mode, root path is `/trpg-saikou/`
+   *  - pages: for GitHub Pages - hash mode, root path is `/TRPGWorld/`
    *  - hash: for servers do not support SPA - hash mode, root path is `/`
    *  - default: for any others (like sox-mini) - history mode, root path is `/`
    */
@@ -14,7 +14,7 @@ function getBase() {
   const { VITE_TARGET_ENV } = process.env;
   switch (VITE_TARGET_ENV) {
     case 'pages':
-      return '/trpg-saikou/';
+      return '/TRPGWorld/';
     default:
       return '/';
   }
